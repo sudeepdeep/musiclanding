34b0823b44ba14ac60b550b361ee14dd35e0756a
developer deepu <sudeepsudeep340@gmail.com>
2026-01-25 17:41:22 +0530

    ui changes v4.0

--- src/components/BannerOne.tsx
+++ src/components/BannerOne.tsx
@@ -1,5 +1,5 @@
 // import React, { useEffect, useState } from "react";
-import { motion, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 import { CalendarIcon, Music } from "lucide-react";
 import { SpotifyIcon } from "../assets/Icons";
 import { mainLogo } from "../utils/constants";
